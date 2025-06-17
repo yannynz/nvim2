b3aa15650bc0890d059a254b0da5809456d801a8
rafoshi <rafa.ioshi@gmail.com>
2025-06-17 13:39:31 -0300

    Base lazyvim

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,28 @@
+return {
+  -- the colorscheme should be available when starting Neovim
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+   {
+    "hrsh7th/nvim-cmp",
+    -- load cmp on InsertEnter
+    event = "InsertEnter",
+    -- these dependencies will only be loaded when cmp loads
+    -- dependencies are always lazy-loaded unless specified otherwise
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      -- ...
+    end,
+  },
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+  { "stevearc/dressing.nvim", event = "VeryLazy" },
+}
